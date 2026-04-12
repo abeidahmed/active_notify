0ee77ec202d6eb0008353bfac66bed0b757f8186
abeidahmed <abeidahmed92@gmail.com>
2026-04-12 11:21:14 +0400

    Test child notifiers override parent carriers

--- test/base_test.rb
+++ test/base_test.rb
@@ -1,13 +1,35 @@
 require "test_helper"
 
 class BaseTest < ActiveSupport::TestCase
+  class Generic < ActiveNotify::Carrier
+  end
+
+  class Email < ActiveNotify::Carrier
+    def deliver_now
+      ChildNotifier.history << :email
+    end
+  end
+
   class BaseNotifier < ActiveNotify::Base
-    deliver_via :email
-    deliver_via :sms
+    deliver_via :email, class_name: "BaseTest::Generic"
+    deliver_via :sms, class_name: "BaseTest::Generic"
   end
 
   class ChildNotifier < BaseNotifier
-    deliver_via :action_cable
+    deliver_via :action_cable, class_name: "BaseTest::Generic"
+    deliver_via :email, class_name: "BaseTest::Email"
+
+    def self.history
+      @history ||= []
+    end
+
+    def self.reset_history
+      @history = []
+    end
+  end
+
+  setup do
+    ChildNotifier.reset_history
   end
 
   test "defines carriers" do
@@ -16,5 +38,12 @@ class BaseTest < ActiveSupport::TestCase
 
   test "inherits carriers from superclass" do
     assert_equal [:email, :sms, :action_cable], ChildNotifier.carriers.keys
+  end
+
+  test "carrier definition overrides parent carrier" do
+    ChildNotifier.deliver_now
+
+    assert_equal 1, ChildNotifier.history.size
+    assert_equal [:email], ChildNotifier.history
   end
 end
